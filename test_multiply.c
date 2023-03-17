0e202a3c117d9377082ed009cb58c21ef05a2dab
kvk-code <75604208+kvk-code@users.noreply.github.com>
2023-03-17 14:48:56 +0530

    unit test case

---
+++ test_multiply.c
@@ -0,0 +1,21 @@
+// test_multiply.c
+#include <stdio.h>
+#include "multiply.h"
+
+void test_multiply() {
+    int a = 5;
+    int b = 3;
+    int expected_result = 15;
+    int result = multiply(a, b);
+
+    if (result == expected_result) {
+        printf("Test multiply() PASSED\n");
+    } else {
+        printf("Test multiply() FAILED. Expected %d, but got %d\n", expected_result, result);
+    }
+}
+
+int main() {
+    test_multiply();
+    return 0;
+}
